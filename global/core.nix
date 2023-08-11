28691fa665e3249f8ed34a8b41540643e2bb2de0
Cassie Cheung <me@soopy.moe>
2023-08-11 20:28:21 +0800

    feat: harden root account

--- global/core.nix
+++ global/core.nix
@@ -1,8 +1,12 @@
-{ ... }: 
+{ pkgs, ... }: 
 {
+  # Set default i18n configuration
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
   };
+
+  # Lock root account
+  users.users.root.shell = pkgs.shadow;
 }
