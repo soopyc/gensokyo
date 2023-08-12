ee9bd02b898a4f53ee2318844fa62b5f48798090
Cassie Cheung <me@soopy.moe>
2023-08-12 19:32:11 +0800

    feat: add tmux options
    * disabled secure socket
    * add plugins

--- global/programs/multiplexers.nix
+++ global/programs/multiplexers.nix
@@ -1,4 +1,4 @@
-{ ... }: 
+{ pkgs, ... }: 
 
 {
   programs.tmux = {
@@ -6,5 +6,16 @@
     newSession = true;
     keyMode = "vi";
     historyLimit = 10000;
+
+    # Rationale: being able to keep sessions open is more important than
+    # security at this point, as I have a very unstable connection and
+    # things may explode any minute.
+    # If I manage to find a way to persist tmux sockets across logouts
+    # without this I would definitely turn this back on.
+    secureSocket = false;
+
+    plugins = with pkgs.tmuxPlugins; [
+      power-theme
+    ];
   };
 }
