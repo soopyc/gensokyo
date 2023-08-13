08a46decaacbf5fe93cc1a521d2681126542c2e1
Cassie Cheung <me@soopy.moe>
2023-08-13 18:26:56 +0800

    feat: enable crash dumping

--- global/core.nix
+++ global/core.nix
@@ -7,6 +7,12 @@
     keyMap = "us";
   };
 
+  # Enable crash dumps globally
+  boot.crashDump = {
+    enable = true;
+    reservedMemory = "128M";
+  };
+
   # Lock root account
   users.users.root.shell = pkgs.shadow;  # basically /bin/nologin
 }
