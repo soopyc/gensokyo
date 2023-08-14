9c2e65484cd37e7a106836da5bdcba9db7dffef9
Cassie Cheung <me@soopy.moe>
2023-08-14 23:15:35 +0800

    chore: default temporarily to maintenance page

--- systems/koumakan/services/static-sites/default.nix
+++ systems/koumakan/services/static-sites/default.nix
@@ -4,8 +4,19 @@
     ./keine.nix
   ];
 
-  services.nginx.virtualHost."_" = {
+  services.nginx.virtualHosts."_" = {
     default = true;
-    locations."/".return = "301 https://gensokyo.soopy.moe";
+    useACMEHost = "global.soopy.moe";
+    # locations."/".return = "301 https://gensokyo.soopy.moe";
+
+    # TODO: remove after bringing back up all the services.
+    locations = {
+      "/".return = "503";
+      "~ ^/(index.html|splash.png)".root = "/srv/www/maintenance";
+    };
+    extraConfig = ''
+    	error_page 503 /index.html;
+    '';
+
   };
 }
