4d1e87d2739f68aa9ae04e89e521d2283350bb6c
Cassie Cheung <me@soopy.moe>
2023-08-14 22:43:02 +0800

    fix: actually finish writing keine's config

--- systems/koumakan/services/static-sites/keine.nix
+++ systems/koumakan/services/static-sites/keine.nix
@@ -1,6 +1,26 @@
 { ... }:
 {
   services.nginx.virtualHosts."keine.soopy.moe" = {
-    
-  }
+    useACMEHost = "global.soopy.moe";
+    addSSL = true;  # Don't force SSL on a mirror (implications TBD)
+
+    root = "/srv/www/keine";
+    location = {
+      "/".extraConfig = ''
+        fancyindex_header "/theme/index_header.html";
+      '';
+      "/theme".alias = "/srv/www/misc/keine/theme";
+      "/static".alias = "/srv/www/misc/keine/theme/static/";
+    };
+
+    extraConfig = ''
+      fancyindex              on;
+      fancyindex_header       "/theme/header.html";
+      fancyindex_footer       "/theme/footer.html";
+      fancyindex_show_path    off;
+      fancyindex_name_length  255;
+      fancyindex_exact_size   off;
+      fancyindex_localtime    on;
+    '';
+  };
 }
