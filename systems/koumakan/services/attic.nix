7a91dbc5c89a190948c7be4dcb6700b588e34ba5
Cassie Cheung <me@soopy.moe>
2023-08-19 15:40:19 +0800

    fix(srv.attic): add chunking config
    why do these not have default settings

--- systems/koumakan/services/attic.nix
+++ systems/koumakan/services/attic.nix
@@ -7,6 +7,21 @@
 
     settings = {
       listen = "127.0.0.1:38191";
+        chunking = {
+        # The minimum NAR size to trigger chunking
+        #   If 0, chunking is disabled entirely for newly-uploaded NARs.
+        #   If 1, all NARs are chunked.
+        nar-size-threshold = 64 * 1024; # 64 KiB
+
+        # The preferred minimum size of a chunk, in bytes
+        min-size = 16 * 1024; # 16 KiB
+
+        # The preferred average size of a chunk, in bytes
+        avg-size = 64 * 1024; # 64 KiB
+
+        # The preferred maximum size of a chunk, in bytes
+        max-size = 256 * 1024; # 256 KiB
+      };
     };
   };
 
