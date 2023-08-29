88e668c4ddf8f5c3c4bb256eb383f62749a4a603
Cassie Cheung <me@soopy.moe>
2023-08-29 19:38:22 +0800

    feat(progs.nix): add substituters and sane options

--- global/programs/nix.nix
+++ global/programs/nix.nix
@@ -7,6 +7,26 @@
       "flakes"
     ];
 
+    substituters = [
+      "https://nonbunary.soopy.moe/gensokyo-koumakan"
+      "https://nonbunary.soopy.moe/gensokyo-satori"
+      "https://nonbunary.soopy.moe/gensokyo-global"
+    ];
+
+    trusted-substituters = [
+      "https://nonbunary.soopy.moe/gensokyo-koumakan"
+      "https://nonbunary.soopy.moe/gensokyo-satori"
+      "https://nonbunary.soopy.moe/gensokyo-global"
+    ];
+
+    trusted-public-keys = [
+      "gensokyo-koumakan:ODEzVQdQ7UEHvPsLWYn7wxN//xS/0lkhjHgfkZlxO4k="
+      "gensokyo-satori:dAbXCUMJP2GZfviFA+yD4+dsxedMqMweOjecAM/zfKk="
+      "gensokyo-global:XiCN0D2XeSxF4urFYTprR+1Nr/5hWyydcETwZtPG6Ec="
+    ];
+
+    fallback = true;
+    connect-timeout = 30;
     max-jobs = "auto";
     auto-optimise-store = true;
   };
