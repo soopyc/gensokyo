492dfaa01808c2aa5dbb2d8223163e92bcef673b
Cassie Cheung <me@soopy.moe>
2023-08-30 15:48:30 +0800

    refactor: use specialArgs instead of underdocumented stuff

--- flake.nix
+++ flake.nix
@@ -25,15 +25,15 @@
     nixosConfigurations = {
       koumakan = lib.nixosSystem {
         system = "x86_64-linux";
+
+        # see docs/tips_n_tricks.md#extra_opts for syntax
+        # see docs/utils.md for functions
+        specialArgs = {
+          # inherit mystia;
+          _utils = (import ./global/utils.nix) { inherit pkgs; };
+        };
+
         modules = [
-          {
-            # see docs/tips_n_tricks.md#extra_opts for syntax
-            # see docs/utils.md for functions
-            _module.args = {
-              _utils = (import ./global/utils.nix) { inherit pkgs; };
-            };
-          }
-          
           lanzaboote.nixosModules.lanzaboote
           attic.nixosModules.atticd
 
