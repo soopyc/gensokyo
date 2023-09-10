522d47e74143f0b3eb65c012f243a64c7b02919f
Cassie Cheung <me@soopy.moe>
2023-09-10 19:23:29 +0800

    fix: don't do cursed stuff here yet
    basically reverts 64a840c

--- global/utils.nix
+++ global/utils.nix
@@ -1,32 +1,17 @@
 # see /docs/utils.md for a usage guide
 {
-  inputs,
-  system,
+  # inputs,
+  # system,
   ...
 }: rec {
-  mkVhost = {extraConfig ? "", ...} @ opts:
+  mkVhost = {...} @ opts:
     {
       # ideally mkOverride/mkDefault would be used, but i have 0 idea how it works.
       forceSSL = true;
       useACMEHost = "global.c.soopy.moe";
       kTLS = true;
     }
-    // opts
-    // {
-      # we do some funny things here
-      locations =
-        opts.locations
-        // {
-          "/_cgi/error/" = {alias = "${inputs.mystia.packages.${system}.staticly}/nginx_error_pages/";};
-        };
-      extraConfig =
-        ''
-          error_page 503 /_cgi/error/503.html;
-          error_page 502 /_cgi/error/502.html;
-          error_page 404 /_cgi/error/404.html;
-        ''
-        + extraConfig;
-    };
+    // opts;
 
   mkSimpleProxy = {
     port,
