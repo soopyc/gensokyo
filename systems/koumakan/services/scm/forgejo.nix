394b64c67ac883fe4263535f35c1ddfb0064749f
Cassie Cheung <me@soopy.moe>
2023-10-21 23:32:16 +0800

    fix: fix forgejo override

--- systems/koumakan/services/scm/forgejo.nix
+++ systems/koumakan/services/scm/forgejo.nix
@@ -4,17 +4,23 @@
   ...
 }: {
   services.forgejo = {
-    package = pkgs.forgejo.overrideAttrs(_: prev: rec {
+    package = pkgs.forgejo.overrideAttrs (_: prev: rec {
       pname = prev.pname + "-unstable";
-      hash = "5f83399d296fffefa5b8feddf23befa811cdecb4";
+      _commit = "5f83399d296fffefa5b8feddf23befa811cdecb4";
 
-      version = "1.20.0+dev-2171-g5f83399d29";
-      src = prev.src.overrideAttrs(_: prev': {
-        rev = hash;
-        hash = lib.fakeHash;
+      version = "1.20.0+dev-g${builtins.substring 0 7 _commit}";
+      src = prev.src.overrideAttrs (_: prev': {
+        rev = _commit;
+        hash = "sha256-s0lJhqO7ikdg1LtP2eJB+BDYxhHUNvMdPAP3mrSL2IU=";
       });
 
-      vendorHash = lib.fakeHash;
+      ldflags =
+        prev.ldflags
+        ++ [
+          "-X main.Version=${version}"
+        ];
+
+      vendorHash = "sha256-pBkQP9TcDGsxWwky05PLI59ERgXgg4s8CljeBxFVx6g=";
     });
   };
 }
