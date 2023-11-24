75ffaa1a5a044acf4954464e7c7e3f697ace966b
Cassie Cheung <me@soopy.moe>
2023-11-25 00:43:51 +0800

    global.editor: unset nvim as default and add hx

--- global/programs/editors.nix
+++ global/programs/editors.nix
@@ -1,8 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   programs.neovim = {
     enable = true;
-    defaultEditor = true;
+    defaultEditor = false;
     viAlias = true;
     vimAlias = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    helix
+  ];
+  environment.variables.EDITOR = "hx";
 }
