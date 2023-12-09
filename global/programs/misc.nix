e89375fed3ac487cbff694b0c3b82d26d5c9c01e
Cassie Cheung <me@soopy.moe>
2023-12-09 13:50:19 +0800

    global.pkgs: add du-dust

--- global/programs/misc.nix
+++ global/programs/misc.nix
@@ -5,19 +5,25 @@
   # To search for a specific package, run this command.
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # junk
     wget
     curl
     xh
 
+    # basic sysadmin utils
     file
     ripgrep
     ack
+    du-dust
 
+    # deno
     deno
 
+    # command runners and utilities
     nvd
     just
 
+    # attic
     attic
   ];
 
