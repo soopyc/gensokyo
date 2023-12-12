ef61c137b8c9b53ce9f4659b1c716eb960a3d4ee
Cassie Cheung <me@soopy.moe>
2023-12-13 02:06:42 +0800

    utils: add default robots.txt file

--- global/utils.nix
+++ global/utils.nix
@@ -18,6 +18,16 @@ in rec {
           alias = "${inputs.mystia.packages.${system}.staticly}/nginx_error_pages/";
         };
 
+        # To override, mkForce {}
+        locations."= /robots.txt" = {
+          alias = mkNginxFile "robots.txt" ''
+            # Please stop hammering my server with 200 requests per **second**.
+            User-Agent: *
+            Disallow: *
+          '';
+          tryFiles = "robots.txt =502";
+        };
+
         extraConfig = ''
           access_log off;
           error_log /var/log/nginx/error.log crit;
@@ -52,6 +62,7 @@ in rec {
       map (x: namespace + lib.optionalString (lib.stringLength namespace != 0) "/" + x) files
     ) (_: value);
 
+  # TODO: we probably don't need to retype tryFiles and alias every single time we use this. this is inherently unintuitive.
   mkNginxFile = filename: contents:
     if (builtins.typeOf contents) != "string"
     then throw "argument $contents must be a string, got '${builtins.typeOf contents}' instead."
