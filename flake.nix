2d4ea1f9e0c180e2df414d3c69979be48d35d5a0
Cassie Cheung <me@soopy.moe>
2023-12-24 23:35:48 +0800

    flake: update flake.nix and flake.lock
    • Added input 'nixpkgs-soopy':
        'github:soopyc/nixpkgs/1bb69f92348b739a9b9cc0764673119ef6428b0a' (2023-12-24)

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-soopy.url = "github:soopyc/nixpkgs/master";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
