62ad615564ae0a9972aaff330da4615ee3e953b9
Cassie Cheung <me@soopy.moe>
2024-01-14 15:36:14 +0800

    feat(koumakan): add assets vhost

---
+++ systems/koumakan/services/static-sites/assets.nix
@@ -0,0 +1,17 @@
+{_utils, ...}: {
+  services.nginx.virtualHosts."assets.soopy.moe" = _utils.mkVhost {
+    root = "/opt/public-assets";
+    locations = {
+      "/".extraConfig = ''
+        expires max;
+        etag on;
+        autoindex on;
+        add_header Cache-Control "public";
+      '';
+
+      "~* /\.(?!well-known)".extraConfig = ''
+        deny all;
+      '';
+    };
+  };
+}
