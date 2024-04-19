da958308adc54f4bcd0cd46894e36f4cdf4272d9
Cassie Cheung <me@soopy.moe>
2024-04-19 18:00:46 +0800

    home: pass traits directly

--- global/home.nix
+++ global/home.nix
@@ -1,7 +1,14 @@
-{...}: {
+{
+  config,
+  ...
+}: {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     verbose = true;
+
+    extraSpecialArgs = {
+      inherit (config.gensokyo) traits;
+    };
   };
 }
