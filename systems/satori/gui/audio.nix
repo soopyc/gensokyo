9767d4bd1d452f263757c0c2fa79edfc5ecf6482
Cassie Cheung <me@soopy.moe>
2024-04-21 04:01:14 +0800

    host(satori): rollback some audio changes and disable alsa suspending

--- systems/satori/gui/audio.nix
+++ systems/satori/gui/audio.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services = {
     pipewire = {
       enable = true;
@@ -11,15 +11,36 @@
         support32Bit = true;
       };
 
-      extraConfig.pipewire = {
-        "10-force-medium-quantum" = {
-          "context.properties" = {
-            "default.clock.quantum" = 1024;
-            "default.clock.min-quantum" = 256;
-            "default.clock.quantum-floor" = 256;
-          };
-        };
-      };
+      # extraConfig = {
+      #   pipewire = {
+      #     "10-force-quantum" = {
+      #       "context.properties" = {
+      #         "default.clock.quantum" = 2048;
+      #         "default.clock.min-quantum" = 1024;
+      #         "default.clock.quantum-floor" = 1024;
+      #       };
+      #     };
+      #   };
+
+      #   pipewire-pulse = {
+      #     "stream.properties" = {
+      #       "node.latency" = "256/48000";
+      #     };
+      #   };
+      # };
+
+      wireplumber.configPackages = [
+        # why aren't these consistent
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/50-disable-alsa-suspend.conf" ''
+          monitor.alsa.rules = [{
+            matches = [{node.name = "~alsa_output.*"}]
+            actions = {
+              update-props = {session.suspend-timeout-seconds = 0}
+            }
+          }]
+        '')
+      ];
     };
   };
+  # systemd.user.services.pipewire-pulse = {};
 }
