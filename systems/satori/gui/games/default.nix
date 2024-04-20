2873f7f76a309a3e0e5d7310a13d721c77a60f12
Cassie Cheung <me@soopy.moe>
2024-04-21 03:48:20 +0800

    host(satori): enable steam

--- systems/satori/gui/games/default.nix
+++ systems/satori/gui/games/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
     ./osu.nix
+    ./steam.nix
   ];
 }
---
+++ systems/satori/gui/games/steam.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: {
+  programs.steam = {
+    enable = true;
+
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
+  };
+}
