77d2d1a3e6352dfd1fc3067b8b57b83151aa616e
Cassie Cheung <me@soopy.moe>
2024-04-20 15:16:58 +0800

    traits: new trait - gaming

--- global/traits/default.nix
+++ global/traits/default.nix
@@ -1,5 +1,7 @@
 {lib, ...}: {
   options.gensokyo.traits = {
     gui = lib.mkEnableOption "graphical programs, related packages and modules";
+
+    games = lib.mkEnableOption "games and etc.";
   };
 }
