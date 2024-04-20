6d7a8d5438cfb721a2d5b128523a3e45e63f0131
Cassie Cheung <me@soopy.moe>
2024-04-20 17:50:13 +0800

    style: autoformat

--- global/upgrade-diff.nix
+++ global/upgrade-diff.nix
@@ -1,5 +1,9 @@
 # Thank you https://github.com/luishfonseca/dotfiles/blob/ab7625ec406b48493eda701911ad1cd017ce5bc1/modules/upgrade-diff.nix
-{lib, pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   system.activationScripts.diff = {
     supportsDryActivation = true;
     text = ''
