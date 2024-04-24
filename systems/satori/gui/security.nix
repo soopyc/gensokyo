0d25b6b971ae2843e83793af424a3d550253ebf5
Cassie Cheung <me@soopy.moe>
2024-04-24 18:05:41 +0800

    host(satori): enable mullvad

--- systems/satori/gui/security.nix
+++ systems/satori/gui/security.nix
@@ -2,4 +2,9 @@
   services.yubikey-agent.enable = true;
   programs.gnupg.agent.pinentryPackage = pkgs.pinentry-qt;
   # FIXME: fix yubikey-agent being stubborn
+
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
 }
