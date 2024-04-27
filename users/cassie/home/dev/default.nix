89e9233a737d2a66fd12c2b8d7a2e91021600d3a
Cassie Cheung <me@soopy.moe>
2024-04-27 17:09:44 +0800

    home(cassie): refactor git config into a new module

--- users/cassie/home/dev/default.nix
+++ users/cassie/home/dev/default.nix
@@ -1,27 +1,5 @@
-{
-  lib,
-  traits,
-  ...
-}: {
-  programs.git = lib.mkMerge [
-    {
-      enable = true;
-      userName = "Cassie Cheung";
-      userEmail = "me@soopy.moe";
-
-      difftastic.enable = true;
-      # delta.enable = true;
-    }
-
-    (lib.mkIf traits.gui {
-      signing = {
-        signByDefault = true;
-        key = builtins.toFile "signing.pub" "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJxpXpPlPEZPfnw2mIuWJEy/C/5h1bb6pIMeFsHAICQ+lLdEkbBSeDXQuA8feLN0MJw8KaB9jqrJbYgFadV/nVA=";
-      };
-
-      extraConfig = {
-        gpg.format = "ssh";
-      };
-    })
+{...}: {
+  imports = [
+    ./git.nix
   ];
 }
