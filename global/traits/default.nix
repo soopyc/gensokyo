9506fa39f60f9854fba0ed5842fbeb4146aca538
Cassie Cheung <me@soopy.moe>
2024-04-27 21:32:39 +0800

    traits: new trait - portable

--- global/traits/default.nix
+++ global/traits/default.nix
@@ -1,7 +1,11 @@
 {lib, ...}: {
   options.gensokyo.traits = {
     gui = lib.mkEnableOption "graphical programs, related packages and modules";
-
     games = lib.mkEnableOption "games and etc.";
+    portable = lib.mkEnableOption ''
+      modules commonly found wih portable devices.
+
+      This includes stuff like Wifi modules and etc
+    '';
   };
 }
