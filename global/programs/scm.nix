a67f62980b0a92e072237bb946651df2a229990a
Cassie Cheung <me@soopy.moe>
2024-05-03 14:03:27 +0800

    home(cassie): add allowed signers

--- global/programs/scm.nix
+++ global/programs/scm.nix
@@ -1,9 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   programs.git = {
     enable = true;
     config = {
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
+      gpg.ssh.allowedSignersFile = pkgs.writeText "soopyc.allowedsigners" ''
+        me@soopy.moe namespaces="git" ${builtins.readFile ../../creds/ssh/auth}
+      '';
     };
   };
 }
