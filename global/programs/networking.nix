8fb4439b516bdc131ef1d37807bc794b8dbcc9bb
Cassie Cheung <me@soopy.moe>
2024-05-04 01:49:41 +0800

    global: configure avahi

--- global/programs/networking.nix
+++ global/programs/networking.nix
@@ -7,12 +7,23 @@ lib.mkMerge [
   {
     networking.networkmanager.enable = true;
     networking.domain = "d.soopy.moe";
+
+    services.avahi = {
+      enable = true;
+      nssmdns = true;
+    };
   }
 
   (lib.mkIf config.gensokyo.traits.portable {
     networking = {
       wireless.iwd.enable = true;
       networkmanager.wifi.backend = "iwd"; # iwd works significantly better than wpa_supplicant. however w_s has hotspot support and iwd doesn't. though w_s' hotspot is like, not usable so whatever.
+    };
+  })
+
+  (lib.mkIf (!config.gensokyo.traits.sensitive) {
+    services.avahi = {
+      publish.enable = true;
     };
   })
 ]
