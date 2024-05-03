d34000b84bf2e0423cf720c64cab5f56a0299031
Cassie Cheung <me@soopy.moe>
2024-05-04 01:40:06 +0800

    traits: add sensitive trait

--- global/traits/default.nix
+++ global/traits/default.nix
@@ -1,5 +1,6 @@
 {lib, ...}: {
   options.gensokyo.traits = {
+    sensitive = lib.mkEnableOption "or selectively disable options specific to security-sensitive systems";
     gui = lib.mkEnableOption "graphical programs, related packages and modules";
     games = lib.mkEnableOption "games and etc.";
     portable = lib.mkEnableOption ''
