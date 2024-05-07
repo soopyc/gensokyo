0ec60f17ee4b9ffd52e4a1f93d45d4446418de78
Cassie Cheung <me@soopy.moe>
2024-05-07 14:07:07 +0800

    host(koumakan): don't apply recommended proxy settings to cno

--- systems/koumakan/services/proxies/cno.nix
+++ systems/koumakan/services/proxies/cno.nix
@@ -8,18 +8,29 @@
 }: {
   services.nginx.virtualHosts."nixpkgs.reverse.proxy.internal.soopy.moe" = _utils.mkVhost {
     locations."/" = {
-      proxyPass = "https://cache.nixos.org";
+      proxyPass = "https://cache.nixos.org/";
+      recommendedProxySettings = false;
 
       basicAuthFile = pkgs.writeText "nixpkgs-proxy-auth" ''
-        balls:$5$.NQ3wV7LOGMoBfio$W5hWgQqz58HkYZefBB45bBVU6lGYDKmuOKZmTY1Ypx/
+        balls:$2y$05$97UTo7JHuyWNS7UeFdiTvuwxZkG0XXO9XklD5TmUKWrL5iMaH5Gq6
+      '';
+
+      extraConfig = ''
+        proxy_set_header Host cache.nixos.org;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        proxy_ssl_session_reuse on;
+        proxy_ssl_server_name on;
+        proxy_ssl_protocols TLSv1.2 TLSv1.3;
       '';
     };
 
     useACMEHost = "proxy.c.soopy.moe";
     extraConfig = lib.mkForce ''
       # store access logs and see who's accessing the proxy, then ban them.
-      access_log /var/log/nginx/cno.access;
-      error_log /var/log/nginx/error.log crit;
+      access_log /var/log/nginx/cno.log;
+      error_log /var/log/nginx/cno.log crit;
     '';
   };
 }
