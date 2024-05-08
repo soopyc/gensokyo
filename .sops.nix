1155fb9da5da10cb383524db20e2d7555f608423
Cassie Cheung <me@soopy.moe>
2024-05-08 12:15:09 +0800

    sops: migrate away from gpg

--- .sops.nix
+++ .sops.nix
@@ -2,17 +2,34 @@ let
   # maintainers
   age.soopyc_pxl7ag = "age1l3qxt6630dzesdclfm3eqgw3uuhwj09dh6typwlwr6clcv0qhfrqgtj2fk";
   # age.soopyc_yub302 = "age1yubikey1qgmfcf0vddslyza7djdekjjk3t3u29d474c5xscmcdye8x3spvhlxxj23xz";
-  pgp.soopyc_yub302 = "8F3B277901484C6EA7E63F82D539637D518022C6";
+  age.soopyc_mbp14 = "age1zkafenrdkkmatjh034yykpzjzzs5fx6kft23jlmsung3dwyufcksds59l2";
 
   # hosts
   age.koumakan = "age18h7hya5terghrwawgpny28swlat2nqkdrfd4clk0svujqlz9xfusd3zeqt";
+  age.satori = "age17sesmv0vs79gc0e2dm66feamph4h6xp4plyatwxkhupucnefnqlsvyk8m3";
+  age.bocchi = "age1w7l663nmqq87e46893dngcy8uxpfeuxlp42a3ma0x4wyuxltjddsecy06p";
 
   everything = [
     {
       age = builtins.attrValues age;
-      pgp = builtins.attrValues pgp;
     }
   ];
+
+  mkHost = name: identities:
+    assert builtins.typeOf identities == "list"; {
+      path_regex = "creds/sops/${name}/.*";
+      key_groups = [
+        {
+          age =
+            [
+              # admin
+              age.soopyc_pxl7ag
+              age.soopyc_mbp14
+            ]
+            ++ identities;
+        }
+      ];
+    };
 in {
   # remember to run `just utils update-sops-config` and `sops updatekeys` after editing.
   creation_rules = [
@@ -21,18 +38,8 @@ in {
       key_groups = everything;
     }
 
-    {
-      path_regex = "creds/sops/koumakan/.*";
-      key_groups = [
-        {
-          age = with age; [
-            soopyc_pxl7ag
-            # soopyc_yub302
-            koumakan
-          ];
-          pgp = [pgp.soopyc_yub302];
-        }
-      ];
-    }
+    (mkHost "koumakan" [age.koumakan])
+
+    (mkHost "bocchi" [age.bocchi])
   ];
 }
