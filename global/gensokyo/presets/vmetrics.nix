b072331afa3154118f7dd90b01cb778898d46dff
Cassie Cheung <me@soopy.moe>
2024-05-08 18:09:20 +0800

    presets(vmetrics): fix unwanted module inclusion

--- global/gensokyo/presets/vmetrics.nix
+++ global/gensokyo/presets/vmetrics.nix
@@ -11,27 +11,35 @@
   };
 in {
   # inb4 this causes conflicts
-  imports = [secrets.generate];
 
-  config = lib.mkIf config.gensokyo.presets.vmetrics {
-    services.prometheus.exporters.node.enable = true;
-    services.vmagent.enable = true;
-    services.vmagent.remoteWrite.url = "https://panopticon.soopy.moe/api/v1/write";
-    services.vmagent.extraArgs = ["-remoteWrite.bearerTokenFile %d/auth_token"];
-    services.vmagent.prometheusConfig = {
-      global.scrape_interval = "30s";
+  config = lib.mkIf config.gensokyo.presets.vmetrics (lib.mkMerge [
+    {
+      services.prometheus.exporters.node.enable = true;
+      services.vmagent.enable = true;
+      services.vmagent.remoteWrite.url = "https://panopticon.soopy.moe/api/v1/write";
+      services.vmagent.extraArgs = ["-remoteWrite.bearerTokenFile %d/auth_token"];
+      services.vmagent.prometheusConfig = {
+        global.scrape_interval = "30s";
 
-      scrape_configs = [
-        {
-          job_name = "node";
-          static_configs = [{targets = ["localhost:9100"];}];
-          relabel_configs = [{target_label = "instance"; replacement = "${hostname}.d.soopy.moe";}];
-        }
+        scrape_configs = [
+          {
+            job_name = "node";
+            static_configs = [{targets = ["localhost:9100"];}];
+            relabel_configs = [
+              {
+                target_label = "instance";
+                replacement = "${hostname}.d.soopy.moe";
+              }
+            ];
+          }
+        ];
+      };
+
+      systemd.services.vmagent.serviceConfig.LoadCredential = [
+        "auth_token:${secrets.get "auth"}"
       ];
-    };
+    }
 
-    systemd.services.vmagent.serviceConfig.LoadCredential = [
-      "auth_token:${secrets.get "auth"}"
-    ];
-  };
+    secrets.generate
+  ]);
 }
