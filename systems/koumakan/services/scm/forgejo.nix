b76a3050f8cf1f037cb4e6ec27cb1698ed673d77
Cassie Cheung <me@soopy.moe>
2024-05-08 14:16:51 +0800

    host(koumakan): return to stable forgejo

--- systems/koumakan/services/scm/forgejo.nix
+++ systems/koumakan/services/scm/forgejo.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  inputs,
   _utils,
   lib,
   config,
@@ -31,7 +30,6 @@ in {
   };
   services.forgejo = {
     enable = true;
-    package = inputs.mystia.packages.${pkgs.system}.forgejoUnstable;
 
     repositoryRoot = "${config.services.forgejo.stateDir}/data/repositories";
 
