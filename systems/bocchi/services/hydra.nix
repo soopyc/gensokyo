aaeb46253ea0919e4e64f9b7b9045f6551f72018
Cassie Cheung <me@soopy.moe>
2024-05-10 02:11:19 +0800

    host(bocchi): configure signing on hydra cache

--- systems/bocchi/services/hydra.nix
+++ systems/bocchi/services/hydra.nix
@@ -1,4 +1,14 @@
-{_utils, ...}: {
+{_utils, config, ...}: let
+  secrets = _utils.setupSecrets config {
+    namespace = "hydra";
+    secrets = ["signing_key"];
+    config = {
+      owner = "hydra";
+    };
+  };
+in {
+  imports = [secrets.generate];
+
   services.hydra = {
     enable = true;
     listenHost = "127.0.0.1";
@@ -6,6 +16,14 @@
     hydraURL = "https://hydra.soopy.moe";
     notificationSender = "hydra@services.soopy.moe";
     smtpHost = "mail.soopy.moe";
+
+    extraConfig = ''
+      email_notification 1
+      compress_build_logs 1
+      binary_cache_secret_key_file ${secrets.get "signing_key"}
+    '';
+
+    # TODO: setup email
   };
 
   services.nginx.virtualHosts."hydra.soopy.moe" = _utils.mkSimpleProxy {
