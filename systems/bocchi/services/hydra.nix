d24eb62f44f9dc4c672a68d1a0f42ed5b9377fff
Cassie Cheung <me@soopy.moe>
2024-05-10 12:43:22 +0800

    host(bocchi): setup hydra emails

--- systems/bocchi/services/hydra.nix
+++ systems/bocchi/services/hydra.nix
@@ -1,14 +1,36 @@
 {_utils, config, ...}: let
   secrets = _utils.setupSecrets config {
     namespace = "hydra";
-    secrets = ["signing_key"];
+    secrets = [
+      "signing_key"
+      "mail/password"
+      "auth/gitea/cassie"
+    ];
     config = {
       owner = config.users.users.hydra-www.name;
       group = config.users.users.hydra-www.group;
+      mode = "0440";
     };
   };
 in {
-  imports = [secrets.generate];
+  imports = [
+    secrets.generate
+
+    (secrets.mkTemplate "hydra-auth.conf" ''
+      <gitea_authorization>
+        cassie = ${secrets.placeholder "auth/gitea/cassie"}
+      </gitea_authorization>
+    '')
+
+    (secrets.mkTemplate "hydra-email.env" ''
+      EMAIL_SENDER_TRANSPORT=SMTP
+      EMAIL_SENDER_TRANSPORT_host=mail.soopy.moe
+      EMAIL_SENDER_TRANSPORT_ssl=starttls
+      EMAIL_SENDER_TRANSPORT_helo=hydra.soopy.moe
+      EMAIL_SENDER_TRANSPORT_sasl_username=hydra@services.soopy.moe
+      EMAIL_SENDER_TRANSPORT_sasl_password=${secrets.placeholder "mail/password"}
+    '')
+  ];
 
   services.hydra = {
     enable = true;
@@ -22,10 +44,15 @@ in {
       email_notification 1
       compress_build_logs 1
       binary_cache_secret_key_file ${secrets.get "signing_key"}
+
+      # Includes
+      Include ${secrets.getTemplate "hydra-auth.conf"}
     '';
 
     # TODO: setup email
   };
+
+  systemd.services.hydra-notify.serviceConfig.EnvironmentFile = secrets.getTemplate "hydra-email.env";
 
   services.nginx.virtualHosts."hydra.soopy.moe" = _utils.mkSimpleProxy {
     port = 3000;
