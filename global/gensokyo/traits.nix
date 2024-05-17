c121110eed36bff1864bd794e61dedbc4c7ca658
Cassie Cheung <me@soopy.moe>
2024-05-17 17:57:46 +0800

    traits: add secure trait

--- global/gensokyo/traits.nix
+++ global/gensokyo/traits.nix
@@ -1,10 +1,11 @@
 {lib, ...}: {
   options.gensokyo.traits = {
     sensitive = lib.mkEnableOption "or selectively disable options specific to security-sensitive systems";
+    secure = lib.mkEnableOption "options for a secure system like secureboot";
     gui = lib.mkEnableOption "graphical programs, related packages and modules";
     games = lib.mkEnableOption "games and etc.";
     portable = lib.mkEnableOption ''
-      modules commonly found wih portable devices.
+      modules commonly found with portable devices.
 
       This includes stuff like Wifi modules and etc
     '';
