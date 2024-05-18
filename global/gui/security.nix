40c473c071ab10b68c974e35cbb06a191bd178ef
Cassie Cheung <me@soopy.moe>
2024-05-18 14:12:33 +0800

    global: disable mullvad

--- global/gui/security.nix
+++ global/gui/security.nix
@@ -8,9 +8,4 @@ lib.mkIf config.gensokyo.traits.gui {
   services.yubikey-agent-socket.enable = true;
   programs.gnupg.agent.pinentryPackage = pkgs.pinentry-qt;
   # FIXME: fix yubikey-agent being stubborn
-
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs.mullvad-vpn;
-  };
 }
