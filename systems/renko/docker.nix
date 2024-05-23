def6b8dfcca514142585337ecf64411207c2cc38
Cassie Cheung <me@soopy.moe>
2024-05-23 20:53:59 +0800

    host(renko): add cassie to docker but don't apply by default

--- systems/renko/docker.nix
+++ systems/renko/docker.nix
@@ -3,4 +3,12 @@
     enable = true;
     storageDriver = "btrfs";
   };
+
+  users.groups.docker.members = [
+    "cassie"
+  ];
+
+  system.activationScripts.lock-docker-group = {
+    text = "gpasswd -R docker";
+  };
 }
