561dae2b171cb1fb89732878e8d46b06319b25b8
Cassie Cheung <me@soopy.moe>
2024-05-29 13:59:19 +0800

    users(builder): don't use nixbld group

--- users/builder.nix
+++ users/builder.nix
@@ -14,10 +14,15 @@ lib.mkIf (!config.gensokyo.traits.sensitive) {
     };
     isNormalUser = false;
     isSystemUser = true;
-    group = "nixbld";
+    # group = "nixbld";
+    # https://github.com/NixOS/nix/blob/946fd29422361e8478425d6aaf9ccae23d7ddffb/src/nix/daemon.cc#L266-L267
+    # https://discourse.nixos.org/t/strange-remote-build-issue/24387/4
+    group = "remote-builder";
 
     # allow builders to actually access nix
     # todo: harden this somehow
     shell = pkgs.zsh;
   };
+
+  users.groups.remote-builder = {};
 }
