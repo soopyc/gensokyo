a480a9570413cd5ce7f27296b3bd8543b5b66d82
Cassie Cheung <me@soopy.moe>
2024-07-20 20:15:54 +0800

    presets(certificates): init

--- global/gensokyo/presets/default.nix
+++ global/gensokyo/presets/default.nix
@@ -2,10 +2,12 @@
   imports = [
     ./vmetrics.nix
     ./nginx.nix
+    ./certificates.nix
   ];
 
   options.gensokyo.presets = {
     vmetrics = lib.mkEnableOption "vmetrics presets";
     nginx = lib.mkEnableOption "nginx presets";
+    certificates = lib.mkEnableOption "boilerplate certificate issuing presets";
   };
 }
