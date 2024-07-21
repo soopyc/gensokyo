ab0ffafc596530bf9014ee9f746ad324a5809889
Cassie Cheung <me@soopy.moe>
2024-07-21 17:40:29 +0800

    host(kita): finalize roundcube config

--- systems/kita/services/mail/roundcube.nix
+++ systems/kita/services/mail/roundcube.nix
@@ -8,6 +8,28 @@
     package = pkgs.roundcube.withPlugins (plugins: with plugins; [carddav contextmenu]);
     dicts = with pkgs.aspellDicts; [en];
     hostName = "webmail.soopy.moe";
+
+    extraConfig = ''
+      // ssl means implicit tls, NOT ssl. see roundcube docs for details.
+      $config['imap_host'] = [
+        'tls://mail.soopy.moe:143' => '(Deprecated) mail.soopy.moe',
+        'ssl://mx2.soopy.moe:993' => 'Gensokyo Mail Exchange',
+      ];
+      // TODO: setup smtp and add related stuff here
+      $config['product_name'] = 'GensoNet Webmail';
+      $config['support_url'] = 'https://soopy.moe';
+      $config['prefer_html'] = false;
+      $config['plugins'] = [
+        'archive',
+        'emoticons',
+        'filesystem_attachments',
+        'hide_blockquote',
+        'identicon',
+        'newmail_notifier',
+        'reconnect',
+        'carddav',
+      ];
+    '';
   };
 
   services.nginx.virtualHosts."webmail.soopy.moe" = _utils.mkVhost {
