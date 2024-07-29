d9f3612b154fcd00669887896082a81dc00725e9
Cassie Cheung <me@soopy.moe>
2024-07-29 22:17:27 +0800

    host(kita): rspamd: configure webui, milter headers and dns

--- systems/kita/services/mail/rspamd.nix
+++ systems/kita/services/mail/rspamd.nix
@@ -12,7 +12,7 @@ in {
   imports = [
     secrets.generate
     (
-      secrets.mkTemplate "rspamd-controller-pwd.conf" ''
+      secrets.mkTemplate "rspamd-controller-pwd.inc" ''
         password = "${secrets.placeholder "controller_passwd"}";
       ''
     )
@@ -20,20 +20,35 @@ in {
   services.rspamd = {
     enable = true;
     postfix.enable = true;
-    locals.redis.text = ''
-      servers = "${config.services.redis.servers.rspamd.unixSocket}";
+    locals = {
+      "redis.conf".text = ''
+        servers = "${config.services.redis.servers.rspamd.unixSocket}";
+      '';
+
+      "milter_headers.conf".text = ''
+        extended_spam_headers = true;
+        use = ["x-spamd-bar", "authentication-results"];
+      '';
+
+      # global options, which is different from sections
+      "options.inc".text = ''
+        dns {
+          nameserver = "127.0.0.1:53";
+        }
+      '';
+    };
+    workers."controller".extraConfig = ''
+      .include(try=false; priority=10) "${secrets.getTemplate "rspamd-controller-pwd.inc"}"
     '';
-    workers."controller".includes = [
-      (secrets.getTemplate "rspamd-controller-pwd.conf")
-    ];
   };
 
-  services.redis.servers.rspamd = {};
+  services.redis.servers.rspamd.enable = true;
   users.groups.redis-rspamd.members = [
     config.users.users.rspamd.name
   ];
 
-  services.nginx.virtualHosts."kita.soopy.moe".locations."/rspamd" = {
+  services.nginx.virtualHosts."_".locations."/rspamd" = {
     proxyPass = "http://localhost:11334"; # maybe expose this to tailnet instead
+    extraConfig = "rewrite /rspamd/(.*) /$1 break;";
   };
 }
