f554a2b80f450c2d9f0996c5a358665924061cf6
Cassie Cheung <me@soopy.moe>
2024-10-19 01:22:15 +0800

    host(koumakan): setup nginx for pds

--- systems/koumakan/services/fediverse/bsky-pds.nix
+++ systems/koumakan/services/fediverse/bsky-pds.nix
@@ -1,11 +1,19 @@
 {
   inputs,
   pkgs,
+  _utils,
   ...
 }: {
   services.bsky-pds = {
     enable = true;
     package = inputs.mystia.packages.${pkgs.system}.bsky-pds;
     settings.PDS_HOSTNAME = "bsky.soopy.moe";
+  };
+
+  services.nginx.virtualHosts.".bsky.soopy.moe" = _utils.mkSimpleProxy {
+    port = 2583;
+    extraConfig = {
+      useACMEHost = "bsky.c.soopy.moe";
+    };
   };
 }
