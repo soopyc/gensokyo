dc34ce9e86bb83a15947ab12373ec76714e05176
Cassie Cheung <me@soopy.moe>
2024-10-21 12:22:25 +0800

    host(koumakan): unmisconfigure pds

--- systems/koumakan/services/fediverse/bsky-pds.nix
+++ systems/koumakan/services/fediverse/bsky-pds.nix
@@ -7,11 +7,14 @@
   services.bsky-pds = {
     enable = true;
     package = inputs.mystia.packages.${pkgs.system}.bsky-pds;
+
+    # because sensible settings are already defined in the module, we can keep this simple :)
     settings.PDS_HOSTNAME = "bsky.soopy.moe";
   };
 
   services.nginx.virtualHosts.".bsky.soopy.moe" = _utils.mkSimpleProxy {
     port = 2583;
+    websockets = true;
     extraConfig = {
       useACMEHost = "bsky.c.soopy.moe";
     };
