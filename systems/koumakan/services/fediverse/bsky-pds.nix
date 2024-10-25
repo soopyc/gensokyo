136a9fd9469428ee878d72d96e6bc1cedcff5c7b
Cassie Cheung <me@soopy.moe>
2024-10-25 17:10:40 +0800

    host(koumakan): pds: add credentials

--- systems/koumakan/services/fediverse/bsky-pds.nix
+++ systems/koumakan/services/fediverse/bsky-pds.nix
@@ -1,15 +1,32 @@
 {
   inputs,
   pkgs,
+  config,
   _utils,
   ...
-}: {
+}: let 
+  secrets = _utils.setupSecrets config {
+    namespace = "pds";
+    secrets = [
+      "email/address"
+      "email/url"
+    ];
+  };
+in {
+  imports = [secrets.generate];
   services.bsky-pds = {
     enable = true;
     package = inputs.mystia.packages.${pkgs.system}.bsky-pds;
 
     # because sensible settings are already defined in the module, we can keep this simple :)
-    settings.PDS_HOSTNAME = "bsky.soopy.moe";
+    settings = {
+      PDS_HOSTNAME = "bsky.soopy.moe";
+      PDS_CONTACT_EMAIL_ADDRESS = "me@soopy.moe";
+    };
+    credentials = {
+      PDS_EMAIL_SMTP_URL = secrets.get "email/url";
+      PDS_EMAIL_FROM_ADDRESS = secrets.get "email/address";
+    };
   };
 
   services.nginx.virtualHosts.".bsky.soopy.moe" = _utils.mkSimpleProxy {
