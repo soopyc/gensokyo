fba1deeeb24430acaa0c3b5cc1532e40ffe52ce6
Cassie Cheung <me@soopy.moe>
2024-11-13 12:34:47 +0800

    global(gui/development): add zed

--- global/gui/development.nix
+++ global/gui/development.nix
@@ -7,7 +7,9 @@
 lib.mkIf config.gensokyo.traits.gui {
   environment.systemPackages = [
     pkgs.vscode
+    pkgs.zed-editor
 
+    # school requirement
     pkgs.eclipses.eclipse-java
     pkgs.mars-mips
   ];
