967f13f912ffacf0cc09b5f8d99888a9f108a388
Cassie Cheung <me@soopy.moe>
2024-11-13 12:43:18 +0800

    chore: move zeditor to hm config

--- global/gui/development.nix
+++ global/gui/development.nix
@@ -7,7 +7,6 @@
 lib.mkIf config.gensokyo.traits.gui {
   environment.systemPackages = [
     pkgs.vscode
-    pkgs.zed-editor
 
     # school requirement
     pkgs.eclipses.eclipse-java
--- users/cassie/home/dev/editors.nix
+++ users/cassie/home/dev/editors.nix
@@ -1,3 +1,34 @@
-{...}: {
-  programs.helix.enable = true;
-}
+{
+  traits,
+  lib,
+  ...
+}:
+lib.mkMerge [
+  {
+    programs.helix.enable = true;
+  }
+
+  (lib.mkIf traits.gui {
+    programs.zed-editor = {
+      enable = true;
+      userSettings = {
+        base_keymap = "VSCode";
+        features = {
+          inline_completion_provider = "none"; # no copilot
+        };
+        telemetry = {
+          metrics = false;
+        };
+        vim_mode = true;
+        ui_font_size = 20;
+        buffer_font_size = 16;
+        buffer_font_family = "Hurmit Nerd Font";
+        theme = {
+          mode = "system";
+          light = "Catppuccin Latte";
+          dark = "Catppuccin Mocha";
+        };
+      };
+    };
+  })
+]
