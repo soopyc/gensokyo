d7650916a1b5dda94652e585b6d6cfbe56205ecb
Cassie Cheung <me@soopy.moe>
2024-11-13 13:56:22 +0800

    home(cassie): update zed settings

--- users/cassie/home/dev/editors.nix
+++ users/cassie/home/dev/editors.nix
@@ -12,21 +12,46 @@ lib.mkMerge [
     programs.zed-editor = {
       enable = true;
       userSettings = {
-        base_keymap = "VSCode";
+        # crap disablement
+        assistant = {
+          version = "1";
+          enabled = false;
+        };
         features = {
           inline_completion_provider = "none"; # no copilot
         };
         telemetry = {
           metrics = false;
         };
-        vim_mode = true;
+
+        # display
         ui_font_size = 20;
         buffer_font_size = 16;
         buffer_font_family = "Hurmit Nerd Font";
+        preferred_line_length = 120;
+        wrap_guides = [120];
+        show_whitespaces = "all";
+        indent_guides = {
+          enabled = true;
+          coloring = "indent_aware";
+        };
         theme = {
           mode = "system";
           light = "Catppuccin Latte";
           dark = "Catppuccin Mocha";
+        };
+        
+        # editing settings
+        base_keymap = "VSCode";
+        hard_tabs = true;
+        vim_mode = true;
+
+        # nix stuff
+        load_direnv = "shell_hook";
+
+        # terminal
+        terminal.env = {
+          "TERM" = "xterm-256color"; # this is not set apparently
         };
       };
     };
