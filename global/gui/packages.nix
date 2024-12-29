7f1e8d7f9efbcb1fa1137652199294e2883c40b0
Cassie Cheung <me@soopy.moe>
2024-12-29 16:36:50 +0800

    global: add ghostty

--- global/gui/packages.nix
+++ global/gui/packages.nix
@@ -2,11 +2,13 @@
   pkgs,
   lib,
   config,
+  inputs,
   ...
 }:
 lib.mkIf config.gensokyo.traits.gui {
   environment.systemPackages = [
     pkgs.vlc
     pkgs.thunderbird
+    inputs.ghostty.packages.${pkgs.system}.default
   ];
 }
