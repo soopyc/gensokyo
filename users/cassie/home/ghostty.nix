b6df43777e5a090f2e5e57ea6ad7f9718e59ede7
Cassie Cheung <me@soopy.moe>
2025-02-09 00:19:20 +0800

    user(cassie): switch to cozettevector, add dynamic font size

--- users/cassie/home/ghostty.nix
+++ users/cassie/home/ghostty.nix
@@ -11,8 +11,8 @@ lib.mkIf traits.gui {
     package = inputs.ghostty.packages.${pkgs.system}.ghostty;
     settings = {
       theme = "catppuccin-latte";
-      font-family = "Hurmit Nerd Font";
-      font-size = 10;
+      font-family = "CozetteVector";
+      font-size = if traits.hidpi then 20 else 10;
       window-decoration = "client";
     };
   };
