f0715091856e3641b52e72e1cb90cab33c9088d6
Cassie Cheung <me@soopy.moe>
2025-02-14 02:13:06 +0800

    host(renko): enable minio

--- systems/renko/configuration.nix
+++ systems/renko/configuration.nix
@@ -2,6 +2,7 @@
   imports = [
     ./gui
     ./development
+    ./services
   ];
 
   gensokyo = {
---
+++ systems/renko/services/minio.nix
@@ -0,0 +1,28 @@
+{
+  _utils,
+  config,
+  ...
+}: let
+  secrets = _utils.setupSecrets config {
+    namespace = "minio";
+    secrets = [
+      "root_user"
+      "root_pass"
+    ];
+  };
+in {
+  imports = [
+    secrets.generate
+    (secrets.mkTemplate "minio.env" ''
+      MINIO_ROOT_USER=${secrets.placeholder "root_user"}
+      MINIO_ROOT_PASSWORD=${secrets.placeholder "root_pass"}
+    '')
+  ];
+
+  services.minio = {
+    enable = true;
+    region = "ap-east-1";
+    listenAddress = ":26531"; # will be proxied by koumakan
+    rootCredentialsFile = secrets.getTemplate "minio.env";
+  };
+}
