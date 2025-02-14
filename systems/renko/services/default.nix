fc58bde956fc08421a21216626cc15eb3a80c40a
Cassie Cheung <me@soopy.moe>
2025-02-14 14:48:24 +0800

    host(renko): monitor minio

--- systems/renko/services/default.nix
+++ systems/renko/services/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./vmagent.nix
     ./minio.nix
   ];
 }
---
+++ systems/renko/services/vmagent.nix
@@ -0,0 +1,32 @@
+{
+  _utils,
+  config,
+  lib,
+  ...
+}: let
+  secrets = _utils.setupSecrets config {
+    namespace = "vmetrics";
+    secrets = ["minio_token"];
+  };
+in {
+  imports = lib.singleton secrets.generate;
+  systemd.services.vmagent.serviceConfig.LoadCredential = [
+    "minio_token:${secrets.get "minio_token"}"
+  ];
+
+  services.vmagent.prometheusConfig.scrape_configs = lib.singleton {
+    job_name = "minio-job";
+    metrics_path = "/minio/v2/metrics/cluster";
+    scheme = "http";
+    static_configs = lib.singleton {targets = lib.singleton "localhost:26531";};
+    relabel_configs = lib.singleton {
+      target_label = "instance";
+      replacement = config.networking.fqdnOrHostName;
+    };
+
+    # https://github.com/NixOS/nixpkgs/issues/367447
+    # https://docs.victoriametrics.com/sd_configs/#scrape_configs
+    # hard coding because we can't use %{ENV_VAR} syntax (yet) when checking.
+    bearer_token_file = "/run/credentials/vmagent.service/minio_token";
+  };
+}
