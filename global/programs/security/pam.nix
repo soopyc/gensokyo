51f5f169fa4b8c19e2f36ba27625f45ad6b2cd06
Cassie Cheung <me@soopy.moe>
2025-02-14 15:26:18 +0800

    global(pam): semi-enable (document) rssh cue

--- global/programs/security/pam.nix
+++ global/programs/security/pam.nix
@@ -6,7 +6,11 @@
 lib.mkMerge [
   (lib.mkIf (!config.gensokyo.traits.sensitive) {
     security.pam = {
-      rssh.enable = true;
+      rssh = {
+        enable = true;
+        # not released yet :moai:
+        # settings.cue = true;
+      };
 
       services.sudo.rssh = true;
     };
