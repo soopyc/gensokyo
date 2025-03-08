25d1dd9182e2de7d07a1e3e20e73c8ac0949a9eb
Cassie Cheung <me@soopy.moe>
2025-03-08 15:19:10 +0800

    user(cassie): atuin tuning

--- users/cassie/home/shell.nix
+++ users/cassie/home/shell.nix
@@ -7,7 +7,11 @@
       update_check = false;
       workspaces = true;
       sync_address = "https://atuin.soopy.moe";
+      filter_mode = "session";
+      enter_accept = true;
+      inline_height = 10;
 
+      daemon.sync_frequency = 10;
       sync.records = true;
     };
   };
