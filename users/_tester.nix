1ba5a1079d9221bcb9953733b5d949a9866c91d7
Cassie Cheung <me@soopy.moe>
2025-03-13 15:11:04 +0800

    global(vm): use virtualisation.vmVarient instead

--- users/_tester.nix
+++ users/_tester.nix
@@ -1,17 +1,8 @@
+{...}:
 {
-  lib,
-  config,
-  options,
-  inputs,
-  ...
-}:
-lib.mkIf (options.virtualisation ? qemu) {
-  sops.secrets."passwords/_tester" = {
-    sopsFile = inputs.self + "/creds/sops/global/passwords.yaml";
-    neededForUsers = true;
-  };
-  users.users._tester = builtins.trace "[vm] building tester user..." {
-    hashedPasswordFile = config.sops.secrets."passwords/_tester".path;
+  virtualisation.vmVariant.users._tester = builtins.warn "[vm] building tester user..." {
+    password = "explode";
     isNormalUser = true;
+    extraGroups = ["wheel"];
   };
 }
