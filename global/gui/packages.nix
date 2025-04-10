cd05f00c7e1e3d108cd1ddf73c4a9d063e3a8818
Cassie Cheung <me@soopy.moe>
2025-04-11 05:54:26 +0800

    global(gui): add flameshot and libnotify

--- global/gui/packages.nix
+++ global/gui/packages.nix
@@ -8,6 +8,8 @@
 lib.mkIf config.gensokyo.traits.gui {
   environment.systemPackages = [
     pkgs.vlc
+    pkgs.flameshot
+    pkgs.libnotify
     pkgs.thunderbird
     inputs.ghostty.packages.${pkgs.system}.default
   ];
