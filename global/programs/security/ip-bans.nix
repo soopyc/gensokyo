eeebf85f3abbf83b8e923b9f0f055379a8e51bfb
Cassie Cheung <me@soopy.moe>
2025-04-12 22:26:06 +0800

    global(ipbans): delete bans on firewall down

--- global/programs/security/ip-bans.nix
+++ global/programs/security/ip-bans.nix
@@ -9,11 +9,22 @@ let
   };
 in
 {
-  networking.firewall.extraCommands = builtins.concatStringsSep "\n" (
+  networking.firewall.extraCommands = builtins.concatStringsSep "\n" ([
+    # i'd be damned if they rename the helper, but i also cba writing it *again*, for the same util to show up
+    # 3 times in the final script.
+    "ip46tables -N gensokyo-blackhole"
+    "ip46tables -I INPUT -j gensokyo-blackhole"
+  ] ++
     lib.flatten (
       lib.mapAttrsToList (
-        family: ips: builtins.map (ip: "${family}tables -w -I INPUT -s ${ip} -j DROP") ips
+        family: ips: builtins.map (ip: "${family}tables -w -I gensokyo-blackhole -s ${ip} -j DROP") ips
       ) banned
     )
   );
+
+  networking.firewall.extraStopCommands = ''
+    ip46tables -D INPUT -j gensokyo-blackhole || true
+    ip46tables -F gensokyo-blackhole || true
+    ip46tables -X gensokyo-blackhole || true
+  '';
 }
