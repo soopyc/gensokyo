25fe31962e4bd16cc94b33db500ba01d0477ad11
Cassie Cheung <me@soopy.moe>
2025-04-12 17:48:51 +0800

    global(ipbans): update list

--- global/programs/security/ip-bans.nix
+++ global/programs/security/ip-bans.nix
@@ -3,6 +3,7 @@ let
   banned = {
     ip = [
       "156.229.232.142" # added 2025-04-10: minecraft server scanner with 30m intervals
+      "156.146.63.199" # added 2025-04-11: minecraft server scanner, found no contact methods
     ];
     ip6 = [ ];
   };
