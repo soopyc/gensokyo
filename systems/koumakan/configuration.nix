7021e7dbf697d47c9684495fe9b87f2d7dd2e021
Cassie Cheung <me@soopy.moe>
2025-04-14 23:06:39 +0800

    host(koumakan): remove anubis module from mystia

--- systems/koumakan/configuration.nix
+++ systems/koumakan/configuration.nix
@@ -4,7 +4,6 @@
     inputs.mystia.nixosModules.fixups
     inputs.mystia.nixosModules.vmauth
     inputs.mystia.nixosModules.bsky-pds
-    inputs.mystia.nixosModules.anubis
     inputs.hydra.nixosModules.hydra
     inputs.knotserver-module.nixosModules.default
 
