1b81f6604d3040b9c794653ee1bd609001605484
Cassie Cheung <me@soopy.moe>
2025-05-10 19:29:50 +0800

    host(renko): gpu passthrough act 1

--- systems/renko/gui/virt.nix
+++ systems/renko/gui/virt.nix
@@ -1,8 +1,18 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.virt-manager.enable = true;
   virtualisation.libvirtd = {
     enable = true;
     qemu.swtpm.enable = true;
   };
+
+  # GPU Passthrough
+  boot.initrd.kernelModules = [
+    "vfio_pci" "vfio" "vfio_iommu_type1" "amdgpu"
+  ];
+  boot.kernelParams = lib.singleton ("vfio-pci.ids=" + lib.concatStringsSep "," [
+    "1002:7480" # GPU video
+    "1002:ab30" # GPU Audio
+  ]);
+  virtualisation.spiceUSBRedirection.enable = true;
 }
