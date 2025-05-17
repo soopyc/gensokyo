637db792e2bb942c91b4cd46006e61dbbff9c46e
Cassie Cheung <me@soopy.moe>
2025-05-17 14:09:10 +0800

    global: add commented vencord overlay

--- global/overlays/default.nix
+++ global/overlays/default.nix
@@ -1,3 +1,14 @@
 inputs: [
   inputs.mystia.overlays.default
+  # (final: prev: {
+  #   vencord = prev.vencord.overrideAttrs (final': {
+  #     version = "1.12.1";
+  #     src = final.fetchFromGitHub {
+  #       owner = "Vendicated";
+  #       repo = "Vencord";
+  #       tag = "v${final'.version}";
+  #       hash = "sha256-Vs6S8N3q5JzXfeogfD0JrVIhMnYIio7+Dfy12gUJrlU=";
+  #     };
+  #   });
+  # })
 ]
