47623341146ed69fc9be37253cb63cf4c923ffbc
Cassie Cheung <me@soopy.moe>
2025-06-21 21:36:46 +0800

    global(gui): add signal

--- global/gui/degeneracy.nix
+++ global/gui/degeneracy.nix
@@ -7,6 +7,7 @@
 lib.mkIf config.gensokyo.traits.gui {
   environment.systemPackages = [
     pkgs.dosage-tracker
+    pkgs.signal-desktop
     (pkgs.discord.override {
       withOpenASAR = true;
     })
