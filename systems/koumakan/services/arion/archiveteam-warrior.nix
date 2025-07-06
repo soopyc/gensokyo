8d6d47cefa7a399f89340056ab6c313cdf67a826
Cassie Cheung <me@soopy.moe>
2025-07-06 15:51:08 +0800

    style: format

--- systems/koumakan/services/arion/archiveteam-warrior.nix
+++ systems/koumakan/services/arion/archiveteam-warrior.nix
@@ -2,7 +2,7 @@
   virtualisation.arion.projects.at-warrior.settings = {
     services.atw.service = {
       image = "atdr.meo.ws/archiveteam/warrior-dockerfile";
-      ports = ["127.0.0.1:35842:8001"];
+      ports = [ "127.0.0.1:35842:8001" ];
       restart = "unless-stopped";
     };
 
