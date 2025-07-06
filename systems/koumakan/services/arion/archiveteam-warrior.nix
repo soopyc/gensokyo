17bdc9ee860eaa6856f1e905c4f8487cfec0c394
Cassie Cheung <me@soopy.moe>
2025-07-06 17:10:50 +0800

    host(koumakan): at-warrior: setup auto update

--- systems/koumakan/services/arion/archiveteam-warrior.nix
+++ systems/koumakan/services/arion/archiveteam-warrior.nix
@@ -3,6 +3,10 @@
     image = "atdr.meo.ws/archiveteam/warrior-dockerfile";
     ports = [ "100.100.16.16:35842:8001" ];
 
+    labels = {
+      "io.containers.autoupdate" = "registry";
+    };
+
     environment = {
       DOWNLOADER = "soopyc";
       WARRIOR_ID = "soopyc-uSoU1YZC";
