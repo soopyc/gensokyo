2aae032008d7e9cbfe88d226e059c379e602da8a
Cassie Cheung <me@soopy.moe>
2025-07-07 19:19:00 +0800

    global(gui): add qtmultimedia to fix something

--- global/gui/kde.nix
+++ global/gui/kde.nix
@@ -8,6 +8,8 @@ lib.mkIf config.gensokyo.traits.gui {
   services.desktopManager.plasma6.enable = true;
 
   environment.systemPackages = with pkgs.kdePackages; [
+    qtmultimedia # this fixes something but i forgot what
+
     kalk
     kdf
     kmime
