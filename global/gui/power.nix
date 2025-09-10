c5cd9606827e38fd3079503e69d2f661cd2d40ac
Cassie Cheung <me@soopy.moe>
2025-09-10 11:56:29 +0800

    global(gui): fix power (disable power config for now)

--- global/gui/power.nix
+++ global/gui/power.nix
@@ -6,13 +6,13 @@
 lib.mkIf config.gensokyo.traits.gui {
   # hopefully eradicate suspend
   services.logind = {
-    settings.Login = {
-      KillUserProcesses = false;
-      HandleSuspendKey = "lock";
-      HandleHibernateKey = "lock";
-      HandleLidSwitch = "lock";
-      IdleAction = "lock";
-    };
+    # settings.Login = {
+    #   KillUserProcesses = false;
+    #   HandleSuspendKey = "lock";
+    #   HandleHibernateKey = "lock";
+    #   HandleLidSwitch = "lock";
+    #   IdleAction = "lock";
+    # };
   };
 
   systemd.targets =
