14b0944e64dd4e73acb60d8512ac0f9b24c610e8
Sophie Cheung <me@soopy.moe>
2025-10-09 18:16:27 +0800

    host(renko): fix actions runner

--- systems/renko/services/forgejo-runner.nix
+++ systems/renko/services/forgejo-runner.nix
@@ -15,14 +15,14 @@ in
   imports = lib.singleton secrets.generate;
 
   services.gitea-actions-runner = {
-    package = pkgs.forgejo-forgejo-actions-runner;
+    package = pkgs.forgejo-actions-runner;
     instances.default = {
       enable = true;
       name = "renko-default";
       url = "https://patchy.soopy.moe";
       tokenFile = secrets.get "tokenFile";
       labels = [
-        "ubuntu-22.04:docker://node:20-bullseye"
+        "debian-trixie:docker://node:24-trixie"
       ];
     };
   };
