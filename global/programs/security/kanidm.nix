de6c5d15c99d4b11b1d9776f1f2eb0b2677ed50b
Sophie Cheung <me@soopy.moe>
2025-10-26 12:21:45 +0800

    global: pin kanidm package

--- global/programs/security/kanidm.nix
+++ global/programs/security/kanidm.nix
@@ -1,6 +1,7 @@
-{
+{ pkgs, ...}: {
   services.kanidm = {
     enableClient = true;
+    package = pkgs.kanidm_1_7;
     clientSettings = {
       uri = "https://serenity.mist-nessie.ts.net";
     };
