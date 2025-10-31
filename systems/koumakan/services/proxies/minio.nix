d8d0e036cb6caa2f5dcc93eb86583a22df71216f
Sophie Cheung <me@soopy.moe>
2025-10-31 10:47:29 +0800

    host(koumakan): fix garage

--- systems/koumakan/services/proxies/minio.nix
+++ systems/koumakan/services/proxies/minio.nix
@@ -2,7 +2,6 @@
   _utils,
   lib,
   config,
-  inputs,
   ...
 }:
 let
@@ -35,51 +34,6 @@ in
   systemd.services.minio.environment = {
     MINIO_BROWSER_REDIRECT_URL = "https://s3.soopy.moe/_static";
     MINIO_BROWSER_LOGIN_ANIMATION = "false";
-  };
-
-  services.nginx.virtualHosts = {
-    "s3.soopy.moe" = _utils.mkSimpleProxy {
-      port = 26531;
-      extraConfig = {
-        extraConfig = ''
-          client_max_body_size 32G;
-        '';
-
-        locations."= /_static" = _utils.mkNginxFile {
-          content = ''
-            <!doctype html>
-            <html lang="en">
-            <head>
-              <title>horrors of gensokyo</title>
-              <style>
-                :root {font-family: "monospace";}
-              </style>
-            </head>
-            <body>
-              <h1>gensokyo filedump - public buckets</h1>
-              <ul>
-                <li><a href="//cache.soopy.moe">nix-cache</a></li>
-                <li>lwjgl-nix</li>
-              </ul>
-            </body>
-            </html>
-          '';
-        };
-      };
-    };
-
-    "cache.soopy.moe" = _utils.mkVhost {
-      locations."/".proxyPass = "http://localhost:26531/nix-cache/";
-
-      locations."= /" = {
-        tryFiles = "/listing.html =500";
-        root = inputs.mystia.packages.x86_64-linux.s3-listing.override {
-          bucketName = "nix-cache";
-          bucketUrl = "https://s3.soopy.moe/nix-cache/";
-          bucketWebsiteUrl = "https://cache.soopy.moe";
-        };
-      };
-    };
   };
 
   systemd.services.vmagent.serviceConfig.LoadCredential = [
--- systems/koumakan/services/storage/garage.nix
+++ systems/koumakan/services/storage/garage.nix
@@ -1,6 +1,7 @@
 {
   _utils,
   config,
+  lib,
   pkgs,
   ...
 }:
@@ -16,6 +17,15 @@ let
 in
 {
   imports = [ secrets.generate ];
+
+  users = {
+    users.garage = {
+      isSystemUser = true;
+      group = "garage";
+    };
+    groups.garage = {};
+  };
+
   services.garage = {
     enable = true;
     package = pkgs.garage_2;
@@ -30,15 +40,15 @@ in
       replication_factor = 1; # we only have the resources for a single node atm.
       compression_level = 4;
 
-      rpc_bind_addr = "100.100.16.16:39931";
-      rpc_public_addr = "koumakan.mist-nessie.ts.net:39931";
-      rpc_secret_file = secrets.get "rpc_secret";
-
       s3_api = {
         s3_region = "ap-east-1";
         api_bind_addr = "[::1]:39930";
         root_domain = ".s3.soopy.moe";
       };
+
+      rpc_bind_addr = "100.100.16.16:39931";
+      rpc_public_addr = "koumakan.mist-nessie.ts.net:39931";
+      rpc_secret_file = secrets.get "rpc_secret";
 
       admin = {
         api_bind_addr = "100.100.16.16:39932";
@@ -46,5 +56,62 @@ in
         metrics_token_file = secrets.get "metrics_token";
       };
     };
+  };
+
+  systemd.services.garage.serviceConfig = {
+    DynamicUser = false; # we need to use a mounted filesystem and systemd explodes when i already have a mountpoint at /var/lib/garage/data.
+    User = config.users.users.garage.name;
+    Group = config.users.groups.garage.name;
+    Restart = "on-failure";
+  };
+
+  services.nginx.virtualHosts.".s3.soopy.moe" = _utils.mkSimpleProxy {
+    port = 39930;
+    extraConfig = {
+      extraConfig = ''
+        client_max_body_size 32G;
+        proxy_max_temp_file_size 0;
+      '';
+
+      locations."= /_static" = _utils.mkNginxFile {
+        content = ''
+          <!doctype html>
+          <html lang="en">
+          <head>
+            <title>horrors of gensokyo</title>
+            <style>
+              :root {font-family: "monospace";}
+            </style>
+          </head>
+          <body>
+            <h1>gensokyo filedump - public buckets</h1>
+            <ul>
+              <li><a href="//cache.soopy.moe">nix-cache</a></li>
+              <li>lwjgl-nix</li>
+            </ul>
+          </body>
+          </html>
+        '';
+      };
+    };
+  };
+
+  systemd.services.vmagent.serviceConfig.LoadCredential = [
+    "garage_token:${secrets.get "metrics_token"}"
+  ];
+
+  services.vmagent.prometheusConfig.scrape_configs = lib.singleton {
+    job_name = "garage-job";
+    scheme = "http";
+    static_configs = lib.singleton { targets = lib.singleton "localhost:39932"; };
+    relabel_configs = lib.singleton {
+      target_label = "instance";
+      replacement = config.networking.fqdnOrHostName;
+    };
+
+    # https://github.com/NixOS/nixpkgs/issues/367447
+    # https://docs.victoriametrics.com/sd_configs/#scrape_configs
+    # hard coding because we can't use %{ENV_VAR} syntax (yet) when checking.
+    bearer_token_file = "/run/credentials/vmagent.service/garage_token";
   };
 }
