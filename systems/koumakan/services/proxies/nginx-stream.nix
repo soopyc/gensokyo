8f3aa830cf930d16d9ffa8f988e9dd252948a209
Sophie Cheung <git@soopy.moe>
2025-12-23 15:38:24 +0800

    host(koumakan): fix nginx stream config

--- systems/koumakan/services/proxies/nginx-stream.nix
+++ systems/koumakan/services/proxies/nginx-stream.nix
@@ -1,7 +1,27 @@
 {
+  systemd.tmpfiles.settings."nginx-stream-log"."/var/log/nginx/stream"."d" = {
+    user = "nginx";
+    group = "nginx";
+    mode = "0750";
+  };
+  systemd.services.nginx.serviceConfig = {
+    # needed for transparent proxying
+    CapabilityBoundingSet = ["CAP_NET_RAW"];
+    AmbientCapabilities = ["CAP_NET_RAW"];
+  };
+
   services.nginx.streamConfig = ''
+    resolver 100.100.100.100;
+    proxy_bind $remote_addr transparent;
     proxy_connect_timeout 1s;
     proxy_timeout 30s;
+
+    log_format basic_stream '$remote_addr [$time_local] '
+                            '$protocol $status $bytes_sent $bytes_received '
+                            '$session_time';
+
+    error_log /var/log/nginx/stream/error.log;
+    access_log /var/log/nginx/stream/access.log basic_stream;
 
     # data
     server {
