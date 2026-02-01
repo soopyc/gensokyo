4b97f6712d3699d1b74c31685b6fca5c3237f380
Sophie Cheung <git@soopy.moe>
2026-02-02 02:54:32 +0800

    host(koumakan): mollysocket: setup vapid key

--- systems/koumakan/services/notifications/mollysocket.nix
+++ systems/koumakan/services/notifications/mollysocket.nix
@@ -1,9 +1,25 @@
-{ _utils, ... }:
+{ _utils, config, ... }:
+let
+  secrets = _utils.setupSecrets config {
+    namespace = "mollysocket";
+    secrets = ["vapid_key"];
+  };
+  in
 {
+  imports = [secrets.generate];
   services.mollysocket = {
     enable = true;
     settings.port = 35153;
-    # i don't think we need vapid stuff
+    # we need vapid stuff else it won't start...
+  };
+
+  systemd.services.mollysocket.serviceConfig = {
+    Environment = [
+      "MOLLY_VAPID_KEY_FILE=%d/vapid_key"
+    ];
+    LoadCredential = [
+      "vapid_key:${secrets.get "vapid_key"}"
+    ];
   };
 
   services.nginx.virtualHosts."ms.soopy.moe" = _utils.mkSimpleProxy {
