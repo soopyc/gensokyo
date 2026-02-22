9c9c1b8326fdc43d2c195488871b1d0ae92d2c24
Sophie Cheung <git@soopy.moe>
2026-02-22 16:43:05 +0800

    host(koumakan): set ipv6 and dn42 addresses

--- systems/koumakan/networking/interface.nix
+++ systems/koumakan/networking/interface.nix
@@ -1,4 +1,26 @@
-{ ... }:
 {
   networking.networkmanager.ethernet.macAddress = "stable";
+
+  networking.interfaces."enp3s0" = {
+    ipv4.addresses = [
+      # dn42
+      {
+        address = "172.20.13.225";
+        prefixLength = 28;
+      }
+    ];
+
+    ipv6.addresses = [
+      {
+        address = "2404:c800:9133:2709:b2c::16";
+        prefixLength = 64;
+      }
+
+      # dn42
+      {
+        address = "fddd:443:3c3c:a5ea::16";
+        prefixLength = 64;
+      }
+    ];
+  };
 }
