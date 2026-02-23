8813f287d61603bf2b2764c88a3a7221afbbad1f
Sophie Cheung <git@soopy.moe>
2026-02-23 14:48:36 +0800

    global(gui): add firefoxpwa

--- global/gui/browser.nix
+++ global/gui/browser.nix
@@ -1,11 +1,14 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 lib.mkIf config.gensokyo.traits.gui {
+  environment.systemPackages = lib.singleton pkgs.firefoxpwa;
   programs.firefox = {
     enable = true;
+    nativeMessagingHosts.packages = lib.singleton pkgs.firefoxpwa;
     # package = pkgs.firefox-devedition;
   };
 }
