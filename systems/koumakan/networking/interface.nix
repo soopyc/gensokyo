0a44047f0ba65b4f6e1a7a06f287e12d3e411364
Sophie Cheung <git@soopy.moe>
2026-02-28 04:21:16 +0800

    host(koumakan): update interface ip addrs

--- systems/koumakan/networking/interface.nix
+++ systems/koumakan/networking/interface.nix
@@ -1,13 +1,15 @@
 {
   systemd.network.networks."50-enp3s0-static-ips" = {
     enable = true;
-    DHCP = "ipv4";
-
     name = "enp3s0";
+    DHCP = "no";
 
     address = [
+      # natted private ip
+      "10.69.2.16/16"
+
       # public internet
-      "2404:c800:9133:270d:b2c::16/64"
+      "2404:c805:33dd:9900::16/64"
 
       # dn42
       "172.20.13.225/28"
