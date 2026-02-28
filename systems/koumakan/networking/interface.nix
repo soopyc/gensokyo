ba2c875e6edf23b052e26c7d07b0163177088b11
Sophie Cheung <git@soopy.moe>
2026-03-01 00:43:00 +0800

    host(koumakan): fix ip attempt 1

--- systems/koumakan/networking/interface.nix
+++ systems/koumakan/networking/interface.nix
@@ -1,19 +1,22 @@
+{ lib, ... }:
 {
   systemd.network.networks."50-enp3s0-static-ips" = {
     enable = true;
     name = "enp3s0";
-    DHCP = "no";
+    DHCP = "ipv6";
 
+    gateway = lib.singleton "10.69.0.1"; # ip4 NAT gateway
     address = [
       # natted private ip
       "10.69.2.16/16"
 
       # public internet
-      "2404:c805:33dd:9900::16/64"
+      "2404:c805:33dd:9900::16/56"
 
       # dn42
-      "172.20.13.225/28"
-      "fddd:443:3c3c:a5ea::16/64"
+      # TODO: move this to another interface
+      # "172.20.13.225/28"
+      # "fddd:443:3c3c:a5ea::16/64"
     ];
   };
 }
