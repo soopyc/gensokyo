c503fb624eb98d1819ff355e45f6c21cda6bec9e
Sophie Cheung <git@soopy.moe>
2026-03-05 22:34:09 +0800

    systems: set new hostId for systems

--- systems/default.nix
+++ systems/default.nix
@@ -7,12 +7,11 @@ let
   utils = import ../global/utils.nix;
 
   mkSystem =
-    hostname: system:
+    hostname: system: hostId:
     lib.nixosSystem {
       specialArgs = {
-        inherit inputs;
+        inherit inputs hostname;
 
-        hostname = hostname;
         _utils = utils { inherit inputs system; };
       };
 
@@ -22,20 +21,28 @@ let
         ./${hostname}/hardware-configuration.nix
 
         {
+          networking = {
+            inherit hostId;
+            hostName = hostname;
+          };
+
           home-manager.extraSpecialArgs = { inherit inputs; };
-          networking.hostName = hostname;
           nixpkgs.hostPlatform = lib.mkDefault system; # ensure we detect conflicts
         }
       ];
     };
 in
 {
-  koumakan = mkSystem "koumakan" "x86_64-linux";
-  satori = mkSystem "satori" "x86_64-linux";
-  renko = mkSystem "renko" "x86_64-linux";
+  # when onboarding a new device, generate a 8-char hostId with openssl rand -hex 4
+  # this is not equal to the machine-id.
+
+  youmu = mkSystem "youmu" "x86_64-linux" "f6050bfd";
+  koumakan = mkSystem "koumakan" "x86_64-linux" "58fb4840";
+  satori = mkSystem "satori" "x86_64-linux" "e386e92b";
+  renko = mkSystem "renko" "x86_64-linux" "760c2249";
 
   # cloud servers
-  kita = mkSystem "kita" "x86_64-linux";
-  ryo = mkSystem "ryo" "x86_64-linux";
+  kita = mkSystem "kita" "x86_64-linux" "05a75d20";
+  # ryo = mkSystem "ryo" "x86_64-linux";
   # nijika = mkSystem "nijika" "aarch64-linux";
 }
