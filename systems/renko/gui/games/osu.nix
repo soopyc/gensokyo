eb4f5731611e557150b318bb49ab833abae6d52b
Sophie Cheung <git@soopy.moe>
2026-03-15 00:34:37 +0800

    system(renko): update osu lazer

--- systems/renko/gui/games/osu.nix
+++ systems/renko/gui/games/osu.nix
@@ -8,8 +8,8 @@ lib.mkIf config.gensokyo.traits.games {
   environment.systemPackages = [
     # this override is crap don't do it
     (pkgs.callPackage ./_osu.nix {
-      version = "2026.119.0-lazer";
-      hash = "sha256-lx8lU9tNXD90rpaKlIyR0C4eSivfmVAJP7Wq+n3Ht08=";
+      version = "2026.305.0-lazer";
+      hash = "sha256-azI3PS5LIVq1H02P1Z4Bny2VFqVLUC6pwCj1UD5HA6g=";
       # nativeWayland = true; # this doesnt window properly
     })
   ];
