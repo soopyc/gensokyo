94a49139fd218f560fd07cce96781a22ce7f2aa7
Sophie Cheung <git@soopy.moe>
2026-03-16 00:36:48 +0800

    chore: doc

--- systems/youmu/services/tftp.nix
+++ systems/youmu/services/tftp.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  # TODO: fix firewall
   systemd.tmpfiles.settings."50-netboot-xyz-files" = {
     "/srv/tftp".d.mode = "0755";
     "/srv/tftp/netbootxyz.amd64.efi"."L+".argument = "${pkgs.netbootxyz-efi}";
