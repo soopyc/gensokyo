626a8138203e59458addb1f4d458a3d1f408e024
Sophie Cheung <git@soopy.moe>
2026-03-17 14:26:58 +0800

    system(youmu): update netboot.xyz version

--- systems/youmu/services/tftp.nix
+++ systems/youmu/services/tftp.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
+let
+  netbootxyz-efi = pkgs.fetchurl {
+    url = "https://github.com/netbootxyz/netboot.xyz/releases/download/3.0.0/netboot.xyz-arm64.efi";
+    hash = "sha256-0i3mKD5t4z3YsvXyNIl0n/4D/pEzoz2meAmqhb+f/9A=";
+  };
+in
 {
-  # TODO: fix firewall
   systemd.tmpfiles.settings."50-netboot-xyz-files" = {
     "/srv/tftp".d.mode = "0755";
-    "/srv/tftp/netbootxyz.amd64.efi"."L+".argument = "${pkgs.netbootxyz-efi}";
+    "/srv/tftp/netbootxyz.amd64.efi"."L+".argument = "${netbootxyz-efi}";
   };
 
   services.atftpd.enable = true;
