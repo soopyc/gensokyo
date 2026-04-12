df1763617769e0ee60c668084e3ca2cfa98c6f7a
Sophie Cheung <git@soopy.moe>
2026-04-13 05:05:08 +0800

    system(youmu): configure nat reflection

--- systems/youmu/networking/routing.nix
+++ systems/youmu/networking/routing.nix
@@ -1,3 +1,4 @@
+{ pkgs, lib, ... }:
 let
   # lanIf = "enp0s31f6";
   wanIf = "wan0";
@@ -9,6 +10,10 @@ in
     tables."gensokyo-nat" = {
       family = "ip";
       content = ''
+        set wan_ip {
+          type ipv4_addr;
+        }
+
         chain prerouting {
           type nat hook prerouting priority dstnat; policy accept;
 
@@ -18,6 +23,9 @@ in
 
           # minecraft
           iif "${wanIf}" tcp dport 25565-25599 dnat to 10.69.2.16
+
+          # partially thanks to https://www.monotux.tech/posts/2024/04/hairpin-nat-nftables/
+          ip saddr 10.69.0.0/16 ip daddr @wan_ip dnat to 10.69.2.16
         }
       '';
     };
@@ -25,5 +33,37 @@ in
     preCheckRuleset = ''
       sed '/iif "/s/${wanIf}/lo/g' -i ruleset.conf
     '';
+  };
+
+  # feels botched, but whatever lmao
+  systemd = {
+    timers."nft-update-wanip" = {
+      timerConfig = {
+        OnUnitActiveSec = "30s";
+      };
+
+      wantedBy = lib.singleton "multi-user.target";
+    };
+
+    services."nft-update-wanip" = {
+      path = with pkgs; [
+        jq
+        iproute2
+        nftables
+      ];
+      script = ''
+        set -euo pipefail
+
+        IP_ADDR=$(ip --json a show ${wanIf} \
+          | jq '.[0].addr_info | map(select(.family == "inet"))[0] | .local' --raw-output)
+
+        nft \
+          flush set ip gensokyo-nat wan_ip\; \
+          add element ip gensokyo-nat wan_ip "{ $IP_ADDR }"
+      '';
+
+      after = lib.singleton "network-online.target";
+      requires = lib.singleton "network-online.target";
+    };
   };
 }
