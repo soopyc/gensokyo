384bfb09af542fa1605eaadeae653d1123163277
Sophie Cheung <git@soopy.moe>
2026-04-19 02:13:53 +0800

    system(koumakan): fix hydra deprecation page

--- systems/koumakan/services/ci/default.nix
+++ systems/koumakan/services/ci/default.nix
@@ -1,25 +1,38 @@
+{ _utils, ... }:
 {
-  services.nginx.virtualHosts."hydra.soopy.moe".locations."/" = {
-    return = ''
-      503 "HTTP/1.0 503 Service Unavailable
-      Server: nginx
-      Connection: close
-      Date: Fri, 17 Apr 2026 20:59:50 +0800
+  services.nginx.virtualHosts."hydra.soopy.moe" = _utils.mkVhost {
+    locations."/" = {
+      # this amount of trolling...
+      root = "/var/empty/nonexistent";
+      extraConfig = ''
+        error_page 404 =503 /_error;
+      '';
+    };
 
-      This hydra instance has been deprecated and will not be coming back in the forseeable future,
-      unless there is a complete rewrite of the software. Due to the inherent weirdness of how
-      the current* version of hydra works, we don't entirely trust it to not spontaneously
-      self-combust during normal operations.
+    locations."= /_error" = _utils.mkNginxFile {
+      filename = "a.txt";
+      content = ''
+        HTTP/1.1 503 Service Unavailable
+        Server: nginx
+        Connection: close
+        Date: Fri, 17 Apr 2026 20:59:50 +0800
 
-      https://cache.soopy.moe is still operating normally. Its public key can be found on the site
-      directly, but is also included here for convenience.
+        This hydra instance has been deprecated and will not be coming back in the
+        forseeable future, unless there is a complete rewrite of the software. Due to the
+        inherent weirdness of how the current* version of hydra works, we don't entirely
+        trust it to not spontaneously self-combust during normal operations.
 
-      cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=
+        https://cache.soopy.moe is still operating normally. Its public key can be found
+        on the site directly, but is also included here for convenience.
 
-      We're sorry for the inconvenience, but we're still working on our minimal replacement. It may
-      not have a web UI, but derivations will still be built and uploaded to our cache.
+        cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=
 
-      *: as of the date specified in this fake http response."
-    '';
+        We're sorry for the inconvenience, but we're still working on our minimal
+        replacement. It may not have a web UI, but derivations will still be built and
+        uploaded to our cache.
+
+        *: as of the date specified in this fake http response.
+      '';
+    };
   };
 }
