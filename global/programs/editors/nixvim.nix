78151f5d781eb9af29f8e6d69517dc5b6833b65b
Sophie Cheung <git@soopy.moe>
2026-04-30 17:24:22 +0800

    global(nvim): more stuff

--- global/programs/editors/nixvim.nix
+++ global/programs/editors/nixvim.nix
@@ -13,6 +13,22 @@
       listchars = "space:\\u00b7,tab:\\u2192 ,eol:\\u23ce,nbsp:\\u237d";
     };
 
+    keymaps =
+      let
+        mkMap = key: action: { inherit key action; };
+      in
+      [
+        (mkMap "<Leader>t" "<cmd>Neotree<cr>")
+        (mkMap "U" "<C-R>")
+      ];
+
+    userCommands = {
+      "Fmt" = {
+        command.__raw = "function() vim.lsp.buf.format() end ";
+        desc = "format the entire document";
+      };
+    };
+
     colorschemes.catppuccin = {
       enable = true;
       settings = {
@@ -21,10 +37,49 @@
     };
 
     plugins = {
+      # auto-session.enable = true;
+      coq-nvim.enable = true;
+      lspconfig.enable = true;
       lualine.enable = true;
-      neo-tree.enable = true;
       telescope.enable = true;
       which-key.enable = true;
+      gitsigns.enable = true;
+      tiny-glimmer.enable = true;
+      rainbow-delimeters.enable = true;
+      fidget.enable = true;
+      treesitter.enable = true;
+
+      tiny-inline-diagnostic = {
+        enable = true;
+
+        settings = {
+          preset = "powerline";
+          options = {
+            throttle = 0;
+            show_source.enabled = true;
+            show_code = true;
+
+            add_messages = {
+              # display_count = true;
+              messages = true;
+              show_multiple_glyphs = true;
+            };
+            multilines = {
+              enabled = true;
+              always_show = true;
+            };
+          };
+        };
+      };
+
+      neo-tree = {
+        enable = true;
+        settings = {
+          filesystem.filtered_items = {
+            visible = true;
+          };
+        };
+      };
 
       mini = {
         enable = true;
@@ -33,7 +88,16 @@
         modules = {
           basics = { };
           icons = { };
-          indentscope = { };
+          tabline = { };
+          comment = {
+            mappings.comment_line = "<C-c>";
+          };
+          indentscope = {
+            draw = {
+              delay = 0;
+              animation.__raw = "function() return 0 end";
+            };
+          };
         };
       };
     };
