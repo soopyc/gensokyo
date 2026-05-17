c0f3db414610049c1c8b7bfed3807b5140227561
Sophie Cheung <git@soopy.moe>
2026-05-17 20:39:33 +0800

    global: upgrade kanidm version

--- global/programs/security/kanidm.nix
+++ global/programs/security/kanidm.nix
@@ -2,7 +2,7 @@
 {
   services.kanidm = {
     enableClient = true;
-    package = pkgs.kanidm_1_9;
+    package = pkgs.kanidm_1_10;
     clientSettings = {
       uri = "https://serenity.mist-nessie.ts.net";
     };
