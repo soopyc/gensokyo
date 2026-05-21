6f9da0d7f478efc63ec19da372463fc7f8515050
Sophie Cheung <git@soopy.moe>
2026-05-21 17:42:47 +0800

    user(cassie): fix gcmm alias

--- users/cassie/home/dev/git.nix
+++ users/cassie/home/dev/git.nix
@@ -5,6 +5,6 @@
   # partially migrated to hjem
   home.shellAliases = {
     # redo previous commit when something explodes, like my key died or something
-    gcmm = "git commit -eF .git/COMMIT_EDITMSG"; # FIXME: strip the thing after ------ 8< ------
+    gcmm = "git commit -evF .git/COMMIT_EDITMSG";
   };
 }
